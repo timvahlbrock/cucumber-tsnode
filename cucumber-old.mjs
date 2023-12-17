031dd2ef124839b677f525b516dc1ec88861f07d
Tim Vahlbrock <tim@vahlbrock.de>
2023-12-17 18:09:24 +0100

    add separate config files

--- cucumber-old.mjs
+++ cucumber-old.mjs
@@ -1,10 +1,6 @@
-import { register } from "node:module";
-import { pathToFileURL } from "node:url";
-
 export default [
     `--format-options '{"snippetInterface": "synchronous"}'`
 ].join(" ");
 
 // eslint-disable-next-line no-undef
 process.env.TS_NODE_PROJECT = "features/tsconfig.json";
-register("ts-node/esm", pathToFileURL("./"));
