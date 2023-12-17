15af14eea4a7f3319770d19766f45cca3c0c32ab
Tim Vahlbrock <tim@vahlbrock.de>
2023-12-17 17:58:01 +0100

    add old registration

--- cucumber.mjs
+++ cucumber.mjs
@@ -1,10 +1,11 @@
-import { register } from "node:module";
-import { pathToFileURL } from "node:url";
 
 export default [
-    `--format-options '{"snippetInterface": "synchronous"}'`
+    `--format-options '{"snippetInterface": "synchronous"}'`,
+    `--import features/stepDefinitions/**/*.ts`,
+    `--import features/world.ts`,
+    `--tags "not @ignore"`
 ].join(" ");
 
 // eslint-disable-next-line no-undef
 process.env.TS_NODE_PROJECT = "features/tsconfig.json";
-register("ts-node/esm", pathToFileURL("./"));
+// register("ts-node/esm", pathToFileURL("./"));
